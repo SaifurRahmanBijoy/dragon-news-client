68403be4be5972040bad7c494c09353761256b5e
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-10-24 01:51:08 +0600

    hii

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 
 const News = () => {
   const news = useLoaderData();
-  console.log(news);
+  // console.log(news);
   const { title, details, image_url, category_id } = news;
   return (
     <Card className="shadow">
@@ -14,7 +14,9 @@ const News = () => {
         <Card.Title className="text-primary">{title}</Card.Title>
         <Card.Text className="text-secondary">{details}</Card.Text>
         <Link to={`/category/${category_id}`}>
-          <Button className="w-100" variant="primary">All news in this category</Button>
+          <Button className="w-100" variant="primary">
+            All news in this category
+          </Button>
         </Link>
       </Card.Body>
     </Card>
